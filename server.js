d946f778cd5649cb568bf04c93a2cd915fa889a8
Thomas Babu <tommy.developer@gmail.com>
2013-11-14 12:03:31 +0800

    Config Express to serve page from the repo views with the jade temmplating

--- server.js
+++ server.js
@@ -2,6 +2,26 @@
 	Will contains all Server Side code for our app
 
 		1)  Serving a single static page using Express
+		2)  Config Express to serve page from the repo views with the jade temmplating
 */
 
 	var express = require("express"), app  = express.createServer();
+
+	// To change express to serve like a client webserver adding public folder which contains JS/CSS/Image files
+
+	app.set('views', __dirname + '/views');
+	app.set('view engine' , 'jade');
+	app.set('view options', { layout : false });
+	app.configure(function() {
+		app.use( express.static( __dirname + '/public'));
+	});
+
+	// Config Express to serve home.jade file and setting express to listen port 3000
+
+	app.get('/', function(req, res){
+		res.render('home.jade');
+	});
+	app.listen(3000);
+
+
+
